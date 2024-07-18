fb892ae64bd45f4ab9809852a807db5c87af67b6
Admin <Admin@LAPTOP-1UOOI6NV>
2024-07-18 12:04:31 +0530

    First Commit

--- src/test/java/Testcase/studentMenu.java
+++ src/test/java/Testcase/studentMenu.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
+
 public class studentMenu {
 
 	public static void main(String[] args) throws InterruptedException {
@@ -57,9 +58,62 @@ public class studentMenu {
         sel8.selectByVisibleText("THANJUR");
         driver.findElement(By.id("pincode-uid21")).sendKeys("622001");
         driver.findElement(By.id("communicationPhoneNo-uid22")).sendKeys("04322");
-        Thread.sleep(3000);        
-        //driver.findElement(By.xpath("//button[@class='btn btn-icon btn-icon-end btn-outline-primary btn-next']")).click();
-       driver.close();
+        Thread.sleep(3000);
+        WebElement clic = driver.findElement(By.xpath("//button[contains(@class,'btn-icon btn-icon-end btn-outline-primary btn-next')]"));
+        driver.executeScript("arguments[0].click()", clic);
+        Thread.sleep(3000);
+        WebElement select9 = driver.findElement(By.id("transportMode-uid23"));
+        Select sel9=new Select(select9);
+        sel9.selectByVisibleText("Parent Pickup");
+        WebElement clic1 = driver.findElement(By.xpath("//button[contains(@class,'btn-icon btn-icon-end btn-outline-primary btn-next')]"));
+        driver.executeScript("arguments[0].click()", clic1);
+        Thread.sleep(3000);
+        WebElement select10 = driver.findElement(By.id("nationality-uid26"));
+        Select sel10=new Select(select10);
+        sel10.selectByVisibleText("INDIAN");
+        WebElement select11 = driver.findElement(By.id("motherTonque-uid27"));
+        Select sel11=new Select(select11);
+        sel11.selectByVisibleText("TAMIL");
+        WebElement select12 = driver.findElement(By.id("religion-uid28"));
+        Select sel12=new Select(select12);
+        sel12.selectByVisibleText("HINDU");
+        WebElement select13 = driver.findElement(By.id("community-uid29"));
+        Select sel13=new Select(select13);
+        sel13.selectByVisibleText("MBC");
+        WebElement select14 = driver.findElement(By.id("caste-uid30"));
+        Select sel14=new Select(select14);
+        sel14.selectByVisibleText("BRAHMIN");
+        driver.findElement(By.id("emergencyContactPersonName-uid48")).sendKeys("Anurag Srivastav");
+        driver.findElement(By.id("emergencyContactPersonNumber-uid49")).sendKeys("9415921222");
+        WebElement clic2 = driver.findElement(By.xpath("//button[contains(@class,'btn-icon btn-icon-end btn-outline-primary btn-next')]"));
+        driver.executeScript("arguments[0].click()", clic2);
+        Thread.sleep(3000);
+        WebElement clic3 = driver.findElement(By.xpath("//button[contains(@class,'btn-icon btn-icon-end btn-outline-primary btn-next')]"));
+        driver.executeScript("arguments[0].click()", clic3);
+        driver.findElement(By.id("fatherName-uid58")).sendKeys("Manoj Joshi");
+        driver.findElement(By.id("motherName-uid66")).sendKeys("Kavita Rao");
+        WebElement clic4 = driver.findElement(By.xpath("//button[contains(@class,'btn-icon btn-icon-end btn-outline-primary btn-next')]"));
+        driver.executeScript("arguments[0].click()", clic4);
+        Thread.sleep(3000);
+        WebElement clic5 = driver.findElement(By.xpath("//button[contains(@class,'btn-icon btn-icon-end btn-outline-primary btn-next')]"));
+        driver.executeScript("arguments[0].click()", clic5);
+        Thread.sleep(3000);
+        String filePath= "C:\\Users\\Admin\\Downloads\\dummy1.jpg "; 
+        driver.findElement(By.xpath("(//input[@type='file'])[7]")).sendKeys(filePath);
+        WebElement clic6 = driver.findElement(By.xpath("//button[contains(@class,'btn-icon btn-icon-end btn-outline-primary btn-next')]"));
+        driver.executeScript("arguments[0].click()", clic6);
+        driver.findElement(By.id("jobStatus-uid79")).sendKeys("None");
+        WebElement clic7 = driver.findElement(By.xpath("//button[contains(@class,'btn-icon btn-icon-end btn-outline-primary btn-next')]"));
+        driver.executeScript("arguments[0].click()", clic7);
+        Thread.sleep(3000);
+        driver.close();
+        
+        /* to save the date in the form
+        WebElement clic2 = driver.findElement(By.xpath("//span[text()='Save']"));
+        driver.executeScript("arguments[0].click()", clic2);
+        */
+       
+        
         
         
         
